b6f7b6692b2d4a354dc2adc774edb70e9c603e35
Richard Carson Derr <cakub6@gmx.com>
2025-02-21 22:09:21 -0500

    feat(issue-85): implement runner helper

--- humus.go
+++ humus.go
@@ -7,11 +7,17 @@
 package humus
 
 import (
+	"bytes"
 	"context"
 	_ "embed"
 	"log/slog"
+	"os"
 	"time"
 
+	"github.com/z5labs/humus/internal"
+
+	"github.com/z5labs/bedrock"
+	"github.com/z5labs/bedrock/config"
 	"go.opentelemetry.io/contrib/bridges/otelslog"
 	"go.opentelemetry.io/contrib/instrumentation/runtime"
 	"go.opentelemetry.io/otel"
@@ -29,7 +35,12 @@ import (
 )
 
 //go:embed default_config.yaml
-var DefaultConfig []byte
+var defaultConfig []byte
+
+// DefaultConfig
+func DefaultConfig() config.Source {
+	return internal.ConfigSource(bytes.NewReader(defaultConfig))
+}
 
 // OTelConfig
 type OTelConfig struct {
@@ -209,4 +220,81 @@ type Config struct {
 // Logger
 func Logger(name string) *slog.Logger {
 	return otelslog.NewLogger(name)
+}
+
+// RunnerOptions are configurable parameters of a [Runner].
+type RunnerOptions struct {
+	errHandler ErrorHandler
+}
+
+// RunnerOption sets a value on [RunnerOptions].
+type RunnerOption interface {
+	ApplyRunnerOption(*RunnerOptions)
+}
+
+type runnerOptionFunc func(*RunnerOptions)
+
+func (f runnerOptionFunc) ApplyRunnerOption(ro *RunnerOptions) {
+	f(ro)
+}
+
+// ErrorHandler allows custom error handling logic to be defined
+// for when the [Runner] encounters an error while building or running
+// a [bedrock.App].
+type ErrorHandler interface {
+	HandleError(error)
+}
+
+// ErrorHandlerFunc is a func type of the [ErrorHandler] interface.
+type ErrorHandlerFunc func(error)
+
+// HandleError implements the [ErrorHandler] inteface.
+func (f ErrorHandlerFunc) HandleError(err error) {
+	f(err)
+}
+
+// OnError registers the given [ErrorHandler] with the [Runner].
+func OnError(eh ErrorHandler) RunnerOption {
+	return runnerOptionFunc(func(ro *RunnerOptions) {
+		ro.errHandler = eh
+	})
+}
+
+// Runner orchestrates the building of a [bedrock.App] and running it.
+type Runner[T any] struct {
+	builder    bedrock.AppBuilder[T]
+	errHandler ErrorHandler
+}
+
+// NewRunner initializes a [Runner].
+func NewRunner[T any](builder bedrock.AppBuilder[T], opts ...RunnerOption) Runner[T] {
+	ro := &RunnerOptions{
+		errHandler: ErrorHandlerFunc(func(err error) {
+			log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{}))
+			log.Error("failed to run", slog.String("error", err.Error()))
+		}),
+	}
+	for _, opt := range opts {
+		opt.ApplyRunnerOption(ro)
+	}
+	return Runner[T]{
+		builder:    builder,
+		errHandler: ro.errHandler,
+	}
+}
+
+// Run builds a [bedrock.App], runs it, and handles any error
+// returned from either of those steps.
+func (r Runner[T]) Run(ctx context.Context, cfg T) {
+	app, err := r.builder.Build(ctx, cfg)
+	if err != nil {
+		r.errHandler.HandleError(err)
+		return
+	}
+
+	err = app.Run(ctx)
+	if err == nil {
+		return
+	}
+	r.errHandler.HandleError(err)
 }
