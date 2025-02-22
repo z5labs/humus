5908f71bd9fca72f7f766bfd8059ea2d4fd6afc8
Richard Carson Derr <cakub6@gmx.com>
2025-02-21 23:26:41 -0500

    fix(issue-85): remove duplicate package comment

--- humus.go
+++ humus.go
@@ -3,7 +3,6 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-// Package humus provides a base config and abstraction for running apps.
 package humus
 
 import (
