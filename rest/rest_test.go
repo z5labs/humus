cf56d03b669966f5f21ae60ee2ee70ab8d6a169c
Richard Carson Derr <cakub6@gmx.com>
2025-02-22 16:26:43 -0500

    test(issue-85): add error case for running

--- rest/rest_test.go
+++ rest/rest_test.go
@@ -8,8 +8,11 @@ package rest
 import (
 	"context"
 	"errors"
+	"fmt"
+	"log/slog"
 	"net"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -79,6 +82,61 @@ func TestBuilder_Build(t *testing.T) {
 
 			_, err := b.Build(context.Background(), hsp)
 			if !assert.ErrorIs(t, err, serverErr) {
+				return
+			}
+		})
+	})
+}
+
+type captureHandler struct {
+	slog.Handler
+	records []slog.Record
+}
+
+func (h *captureHandler) Handle(ctx context.Context, record slog.Record) error {
+	h.records = append(h.records, record)
+	return nil
+}
+
+func TestRun(t *testing.T) {
+	t.Run("will handle error", func(t *testing.T) {
+		t.Run("if the http port is not a uint", func(t *testing.T) {
+			r := strings.NewReader(`
+http:
+  port: -1`)
+
+			b := func(ctx context.Context, cfg Config) (*Api, error) {
+				return nil, nil
+			}
+
+			logHandler := &captureHandler{
+				Handler: slog.Default().Handler(),
+			}
+
+			Run(r, b, LogHandler(logHandler))
+
+			records := logHandler.records
+			if !assert.Len(t, records, 1) {
+				return
+			}
+
+			record := records[0]
+			var caughtErr error
+			record.Attrs(func(a slog.Attr) bool {
+				if a.Key != "error" {
+					return true
+				}
+
+				v := a.Value.Any()
+				err, ok := v.(error)
+				if !ok {
+					caughtErr = fmt.Errorf("expected attr to be error: %v", a.Value)
+					return false
+				}
+				caughtErr = err
+				return false
+			})
+			if !assert.Error(t, caughtErr) {
 				return
 			}
 		})
