e8372d8b3df5e96c82d20952e33aa9fc7ae2d8ae
Richard Carson Derr <cakub6@gmx.com>
2025-02-22 15:44:00 -0500

    test(issue-85): added case for default config

--- config_test.go
+++ config_test.go
@@ -13,12 +13,35 @@ import (
 	"github.com/z5labs/humus/config"
 
 	"github.com/stretchr/testify/assert"
+	bedrockcfg "github.com/z5labs/bedrock/config"
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"google.golang.org/grpc"
 )
+
+func TestConfig_InitializeOTel(t *testing.T) {
+	t.Run("will not return an error", func(t *testing.T) {
+		t.Run("with the default parameters", func(t *testing.T) {
+			m, err := bedrockcfg.Read(DefaultConfig())
+			if !assert.Nil(t, err) {
+				return
+			}
+
+			var cfg Config
+			err = m.Unmarshal(&cfg)
+			if !assert.Nil(t, err) {
+				return
+			}
+
+			err = cfg.InitializeOTel(context.Background())
+			if !assert.Nil(t, err) {
+				return
+			}
+		})
+	})
+}
 
 func TestTraceProviderInitializer(t *testing.T) {
 	t.Run("will return an error", func(t *testing.T) {
