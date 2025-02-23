99e0d157af0e941b611d868166ad616257d0a985
Richard Carson Derr <cakub6@gmx.com>
2025-02-22 20:17:52 -0500

    doc(issue-85): use doc file pattern

--- rest/rpc/rpc.go
+++ rest/rpc/rpc.go
@@ -3,7 +3,6 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-// Package rpc helps users implement [http.Handler]s using a RPC style interface.
 package rpc
 
 import (
