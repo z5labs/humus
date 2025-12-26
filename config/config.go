42bfcd1c7a53f65bc874e7735d47c27782a396f1
Richard Carson Derr <cakub6@gmx.com>
2025-12-26 14:11:51 -0500

    docs(issue-437): add package docs for config package

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+// Package config provides abstractions for reading configuration values from various sources.
 package config
 
 import (
